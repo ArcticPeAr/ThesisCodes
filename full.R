1b0ba698a5f1503a6dfe500b31c544f2b5a20bb5
ArcticPeAr <peterarnesen@protonmail.com>
2022-04-07 00:01:13 +0200

    Change dot size for fica-plots

--- full.R
+++ full.R
@@ -173,50 +173,50 @@ library(RColorBrewer)
 ficaSMergePAMSansNA <- ficaSMerge[!(ficaSMerge$PAM50=="NA"),]
 
 figficaSPAM <- ggplot(ficaSMerge, aes(V1, V2))
-figficaSPAM + geom_point(aes(colour = factor(PAM50)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSPAM + geom_point(aes(colour = factor(PAM50)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 figficaSPAMSansNA <- ggplot(ficaSMergePAMSansNA, aes(V1, V2))
-figficaSPAMSansNA + geom_point(aes(colour = factor(PAM50)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSPAMSansNA + geom_point(aes(colour = factor(PAM50)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 ################# GGPLOT2 for ER.Status ################
 
 ficaSMergeERSansNA <- ficaSMerge[!(ficaSMerge$ER.Status=="NA"),]
 
 figficaSER <- ggplot(ficaSMerge, aes(V1, V2))
-figficaSER + geom_point(aes(colour = factor(ER.Status)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSER + geom_point(aes(colour = factor(ER.Status)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 figficaSERSansNA <- ggplot(ficaSMergeERSansNA, aes(V1, V2))
-figficaSERSansNA + geom_point(aes(colour = factor(ER.Status)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSERSansNA + geom_point(aes(colour = factor(ER.Status)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 
 ################# GGPLOT2 for PAM50_genefu ################
 ficaSMergePGSansNA <- ficaSMerge[!(ficaSMerge$PAM50_genefu=="NA"),]
 
 figficaSPG <- ggplot(ficaSMerge, aes(V1, V2))
-figficaSPG + geom_point(aes(colour = factor(PAM50_genefu)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSPG + geom_point(aes(colour = factor(PAM50_genefu)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 figficaSPGSansNA <- ggplot(ficaSMergePGSansNA, aes(V1, V2))
-figficaSPGSansNA + geom_point(aes(colour = factor(PAM50_genefu)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSPGSansNA + geom_point(aes(colour = factor(PAM50_genefu)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 
 ################# GGPLOT2 for PR.Status ################
 ficaSMergePRSansNA <- ficaSMerge[!(ficaSMerge$PR.Status=="NA"),]
 
 figficaSPR <- ggplot(ficaSMerge, aes(V1, V2))
-figficaSPR + geom_point(aes(colour = factor(PR.Status)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSPR + geom_point(aes(colour = factor(PR.Status)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 figficaSPRSansNA <- ggplot(ficaSMergePRSansNA, aes(V1, V2))
-figficaSPRSansNA + geom_point(aes(colour = factor(PR.Status)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSPRSansNA + geom_point(aes(colour = factor(PR.Status)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 
 ################# GGPLOT2 for HER2.Final.Status ################
 ficaSMergeHER2SansNA <- ficaSMerge[!(ficaSMerge$HER2.Final.Status=="NA"),]
 
 figficaSHER2 <- ggplot(ficaSMerge, aes(V1, V2))
-figficaSHER2 + geom_point(aes(colour = factor(HER2.Final.Status)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSHER2 + geom_point(aes(colour = factor(HER2.Final.Status)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 figficaSHER2SansNA <- ggplot(ficaSMergeHER2SansNA, aes(V1, V2))
-figficaSHER2SansNA + geom_point(aes(colour = factor(HER2.Final.Status)), size = 2) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
+figficaSHER2SansNA + geom_point(aes(colour = factor(HER2.Final.Status)), size = 5) + scale_color_brewer(palette = "Set1") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
 
 
 ######################################################
